3dd231aa9a6f29023ab25a1eb7d36eb4b5726bc7
Amena Faruqi <phrkvg@login01.avon.hpc>
2023-06-22 15:01:57 +0100

    remove plot_type arg

--- fargo_analysis/2Dsurface_density.py
+++ fargo_analysis/2Dsurface_density.py
@@ -1,6 +1,5 @@
-from numpy import*
+import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as plt
 import matplotlib.ticker as ticker
 from pylab import*
 import argparse
@@ -18,27 +17,23 @@ from matplotlib import rcParams
 import matplotlib.ticker as mtick
 
 plt.style.use('default')
-plt.style.use(['../styles/presentation.mplstyle', '../styles/darkbg.mplstyle'])
+plt.style.use(['../styles/publication.mplstyle'])
+wd = "/home/astro/phrkvg/simulations/planet_growth/lowres_models"
 
 #____________________________________PLOTTING FUNCTIONS ____________________________________#
-
-def fmt(x, pos):
-    a, b = '{:.1e}'.format(x).split('e')
-    b = int(b)
-    return r'${} \times 10^{{{}}}$'.format(a, b)
                                                                                                       
 # Plot the dust surface mass density at any one time                                                   
-def plot_surf_dens(simdir,dustnums,outputnumber,plottype,lin_scaling,cbmin,cbmax,plot_gas,beam,plot_planet):
+def plot_surf_dens(simdir,dustnums,outputnumber,lin_scaling,cbmin,cbmax,plot_gas,beam,plot_planet):
 # Plots a 2D graph of the dust surface density                
     filepaths = []
 
     if plot_gas in ["yes", "y"]:
-        filepaths.append(f'/home/astro/phrkvg/simulations/planet_growth/{simdir}/gasdens{outputnumber}.dat')
+        filepaths.append(f'{wd}/{simdir}/gasdens{outputnumber}.dat')
 
     for dustnum in dustnums:
-        filepaths.append(f'/home/astro/phrkvg/simulations/planet_growth/{simdir}/dustdens{dustnum}_{outputnumber}.dat')
+        filepaths.append(f'{wd}/{simdir}/dustdens{dustnum}_{outputnumber}.dat')
 
-    params_file = f'/home/astro/phrkvg/simulations/planet_growth/{simdir}/variables.par'
+    params_file = f'{wd}/{simdir}/variables.par'
     params_dict = {}
     param_lines = open(params_file).readlines()
     for line in param_lines:
@@ -52,7 +47,7 @@ def plot_surf_dens(simdir,dustnums,outputnumber,plottype,lin_scaling,cbmin,cbmax
 
     for file_i, filepath in enumerate(filepaths):
         print(f"Plotting {filepath.split('/')[-1]}...")
-        surfdens = fromfile(filepath).reshape(nrad,nphi)/(1.125e-7)
+        surfdens = np.fromfile(filepath).reshape(nrad,nphi)/(1.125e-7)
         if  np.isnan(cbmin):
             vmin = np.percentile(surfdens, 10)
         else: 
@@ -74,181 +69,165 @@ def plot_surf_dens(simdir,dustnums,outputnumber,plottype,lin_scaling,cbmin,cbmax
     #    cbar.set_label('dust surface density')
     #    show()
 
-        if (plottype == "pcontour"):
-            phi = linspace(0,2*pi,nphi+1)
-            r_cells = loadtxt(f'/home/astro/phrkvg/simulations/planet_growth/{simdir}/domain_y.dat')[3:-3]             # ignore ghost cells
-            radii = array([(r_cells[n]+r_cells[n+1])/2 for n in range(len(r_cells)-1)])
+        phi = np.linspace(0,2*np.pi,nphi+1)
+        r_cells = np.loadtxt(f'{wd}/{simdir}/domain_y.dat')[3:-3]             # ignore ghost cells
+        radii = np.array([(r_cells[n]+r_cells[n+1])/2 for n in range(len(r_cells)-1)])
 
-            # radii=loadtxt(f'/home/astro/phrkvg/simulations/planet_growth/{simdir}/used_rad.dat')
-            r,theta=meshgrid(radii,phi)
-            
-            dens_first_wedge = surfdens[:,0].reshape(nrad,1)
-            # print(dens_first_wedge.shape)
-            dens_additional = concatenate((surfdens[:,:],dens_first_wedge),axis=1)
+        # radii=loadtxt(f'/home/astro/phrkvg/simulations/planet_growth/{simdir}/used_rad.dat')
+        r,theta=np.meshgrid(radii,phi)
+        
+        dens_first_wedge = surfdens[:,0].reshape(nrad,1)
+        # print(dens_first_wedge.shape)
+        dens_additional = np.concatenate((surfdens[:,:],dens_first_wedge),axis=1)
 
-            x = r*cos(theta)
-            y = r*sin(theta)
+        x = r*np.cos(theta)
+        y = r*np.sin(theta)
 
-            fig = plt.figure()
+        fig = plt.figure()
 
-    #        plt.rcParams['axes.facecolor'] = 'none'
+#        plt.rcParams['axes.facecolor'] = 'none'
 
-            if (lin_scaling not in ["no", "n"]):
-                plt.pcolormesh(x,y,dens_additional.T,cmap=cm.Oranges_r,shading="auto",vmin=vmin, vmax=vmax)
-    #            plt.pcolormesh(x,y,dens_additional.T,cmap=cm.Oranges_r,vmin=cbmin,vmax=cbmax)
-                cbar_label = '$\Sigma$ [$g/cm^{2}$] '
-
-            else:
-                im = plt.pcolormesh(x,y,log10(dens_additional.T),cmap=cm.Oranges_r,shading="auto",vmin=log10(vmin), vmax=log10(vmax))
-                im.set_rasterized(True)
-                cbar_label = 'log $\Sigma$ [$g/cm^{2}$]'
-
-            # cbar = colorbar(format=ticker.FuncFormatter(fmt))
-            cbar = colorbar()
-            cbar.set_label(cbar_label)
-            plt.xlabel('x [AU]')
-            plt.ylabel('y [AU]')
-
-            if plot_planet not in ["no", "n"]:
-                planet_data = loadtxt(f"/home/astro/phrkvg/simulations/planet_growth/{simdir}/planet0.dat")
-                xp, yp = planet_data[outputnumber][1], planet_data[outputnumber][2]
-                plt.scatter([-xp], [-yp], color='g', marker='.')
-            # plt.xlim(-50,50)
-            # plt.ylim(-50,50)
-            plt.tight_layout()
-
-    #        cbar.ax.yaxis.set_tick_params(color='white')
-    #        cbar.ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
-            # plt.setp(plt.getp(cbar.ax.axes, 'yticklabels'))#, color='w')
-    #        cbar.ax.set_xticklabels(color='white')
-    #        cbar.ax.get_ticklabels('white')
-    #        spines['top'].set_color('red')
-    #        ax1.spines['top'].set_color('red')
-    #        ax1.set_xlabel('X-axis')
-    #        ax1.xaxis.label.set_color('red')
-            
-
-    #        ax1 = plt.gca()
-            # ax = fig.add_subplot(111)
-            # plt.axis('scaled')
-            # ax.set_xlabel('x [code units]')
-            # ax.set_ylabel('y [code units]')
-            # ax.set_xlim([-1.3,1.3])
-            # ax.set_ylim([-1.3,1.3])
-    #        ax.tick_params(axis='y', which='both', color='w')
-    #        ax.xaxis.label.set_color('white')
-    #        ax.yaxis.label.set_color('white')
-    #        ax.xticks.set_color('red')
-    #        ax.tick_params(axis='x', colors='red')
-
-    #        for spine in ax.axes.spines.values():
-    #            spine.set_edgecolor('white')
-
-    #        [t.set_color('white') for t in ax.xaxis.get_ticklines()]
-    #        [t.set_color('white') for t in ax.yaxis.get_ticklines()]
-
-
-    #        [t.set_color('white') for t in ax.xaxis.get_ticklabels()]
-    #        [t.set_color('white') for t in ax.yaxis.get_ticklabels()]
-    #        [t.set_color('white') for t in cbar.set_ticklabels()]
-    #        cbar.ax.label.set_color('white')
-    #        [t.set_color('white') for t in cbar.ax.yaxis.set_tick_params()]
-    #        cbar.ax.yaxis.set_tick_params(color='white')
-    #        cbar.tick(color='white')
-    #        cbar.outline.set_edgecolor('white')
-    #        cbar.color = 'white'
-
-    #        for tick in cbar.get_ticklabels():
-    #            tick.set_color('red')
-
-    # Uncomment the following text, as well as any future references to ax2, if a second plot is needed with the azimuthal averaged sigmad and four slices of the disc
-    #        ax2 = fig.add_subplot(212)
-    #        plt.figure()
-    #        ax2 = plt.gca()
-    #        ax2.set_xlabel('r')
-    #        ax2.set_ylabel("dust surface density")
-    #        sigmad_tot = sum(surfdens,axis=1)
-    #        sigmad_azi = sigmad_tot/nphi
-
-
-            r_celledge = [None]*nrad
-            ncelledge = nrad+1
-    #        print type(ncelledge), ncelledge
-
-    #        filepath_r = 'link/used_rad.dat'
-    #        f = open(filepath_r,'r')
-    #        lines = f.readlines()
-    #        print type(lines), lines
-
-    #        for line in xrange(ncelledge-1):
-    #            data = line.split()
-    #            r_celledge.append(float(data[0]))
-    #
-    #            r_celledge.append(
-
-            rcell = [None] * (len(radii)-1)
-            for i in range(len(radii)-1):
-    #            rcell[i] = (radii[i]*radii[i+1])**0.5
-                rcell[i] = (radii[i]+radii[i+1])/2.
-
-    #        print rcell
-    #        print len(rcell), len(sigmad_azi)
-
-    #        ax2.plot(rcell,sigmad_azi)
-    
-            for i in range(4):
-                j = int(nphi/4.*i)
-    #            print nx/4.*i, j
-                surfdensi = [None]*nrad
-                for k in range(nrad):
-    #                print "inside k = ",k
-    #                print surfdens
-    #                surfdens = []
-    #                print type(surfdens)                
-    #                print surfdens[0,0], type(surfdens[0,0])
-    #                sys.exit()
-    #                surfdensi = float(surfdens[k,j])
-    #                print surfdensi
-                    surfdensi[k] = surfdens[k,j]
-    #            print len(rcell), len(surfdensi)
-    #            ax2.plot(rcell,surfdensi)
-
-            if (beam in ["yes", "y"]):
-                # Put the image in cartesian co-ordinates
-                interp = RectBivariateSpline(np.linspace(0,2*np.pi,nphi+1),rcell,dens_additional.T,kx=1,ky=1)
-                npix=2048
-                x_im=np.linspace(-3,3,npix)
-                y_im=np.linspace(-3,3,npix)
-                X_im,Y_im=np.meshgrid(x_im,y_im)
-                R_im=np.sqrt(X_im**2+Y_im**2)
-                Theta_im=np.arctan2(Y_im,X_im)+np.pi
-                image=interp(Theta_im,R_im,grid=False)
-
-                # Convolve with a gaussian and plot
-                sigma = 50.0
-                convolved=scipy.ndimage.filters.gaussian_filter(image,sigma)
-                fig3 = plt.figure()
-                ax3 = fig3.add_subplot(111) 
-    #            plt.pcolormesh(x,y,log10(dens_additional.T),cmap=cm.Oranges_r,vmin=cbmin,vmax=cbmax)
-                print(image)
-                print(type(image), len(image), shape(image), shape(x), shape(y), shape(x_im), shape(y_im))
-                plt.pcolormesh(-x_im,-y_im,log10(convolved),cmap=cm.Oranges_r,vmin=vmin,vmax=vmax)
-    #            plt.contourf(image, levels=10)
-                ax3.set_xlim([-2.5,2.5])
-                ax3.set_ylim([-2.5,2.5])
-                cbar_label = 'log surface density [code units] (convolved)'
-                cbar = colorbar()
-                cbar.set_label(cbar_label,color='black')
-
+        if (lin_scaling not in ["no", "n"]):
+            plt.pcolormesh(x,y,dens_additional.T,cmap=cm.Oranges_r,shading="auto",vmin=vmin, vmax=vmax)
+#            plt.pcolormesh(x,y,dens_additional.T,cmap=cm.Oranges_r,vmin=cbmin,vmax=cbmax)
+            cbar_label = '$\Sigma$ [$g/cm^{2}$] '
 
         else:
-            plt.figure()
-            imshow(log10(surfdens),origin='lower',cmap=cm.Oranges,aspect='auto',vmin=vmin,vmax=vmax)
-            cbar = colorbar()
-            cbar.set_label('surface density') 
+            im = plt.pcolormesh(x,y,np.log10(dens_additional.T),cmap=cm.Oranges_r,shading="auto",vmin=np.log10(vmin), vmax=np.log10(vmax))
+            im.set_rasterized(True)
+            cbar_label = 'log $\Sigma$ [$g/cm^{2}$]'
+
+        # cbar = colorbar(format=ticker.FuncFormatter(fmt))
+        cbar = plt.colorbar()
+        cbar.set_label(cbar_label)
+        plt.xlabel('x [AU]')
+        plt.ylabel('y [AU]')
+
+        if plot_planet not in ["no", "n"]:
+            planet_data = np.loadtxt(f"/home/astro/phrkvg/simulations/planet_growth/{simdir}/planet0.dat")
+            xp, yp = planet_data[outputnumber][1], planet_data[outputnumber][2]
+            plt.scatter([-xp], [-yp], color='g', marker='.')
+        # plt.xlim(-50,50)
+        # plt.ylim(-50,50)
+        plt.tight_layout()
+
+#        cbar.ax.yaxis.set_tick_params(color='white')
+#        cbar.ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
+        # plt.setp(plt.getp(cbar.ax.axes, 'yticklabels'))#, color='w')
+#        cbar.ax.set_xticklabels(color='white')
+#        cbar.ax.get_ticklabels('white')
+#        spines['top'].set_color('red')
+#        ax1.spines['top'].set_color('red')
+#        ax1.set_xlabel('X-axis')
+#        ax1.xaxis.label.set_color('red')
+        
+
+#        ax1 = plt.gca()
+        # ax = fig.add_subplot(111)
+        # plt.axis('scaled')
+        # ax.set_xlabel('x [code units]')
+        # ax.set_ylabel('y [code units]')
+        # ax.set_xlim([-1.3,1.3])
+        # ax.set_ylim([-1.3,1.3])
+#        ax.tick_params(axis='y', which='both', color='w')
+#        ax.xaxis.label.set_color('white')
+#        ax.yaxis.label.set_color('white')
+#        ax.xticks.set_color('red')
+#        ax.tick_params(axis='x', colors='red')
+
+#        for spine in ax.axes.spines.values():
+#            spine.set_edgecolor('white')
+
+#        [t.set_color('white') for t in ax.xaxis.get_ticklines()]
+#        [t.set_color('white') for t in ax.yaxis.get_ticklines()]
 
 
-    #    set_axis_bgcolor('red')
+#        [t.set_color('white') for t in ax.xaxis.get_ticklabels()]
+#        [t.set_color('white') for t in ax.yaxis.get_ticklabels()]
+#        [t.set_color('white') for t in cbar.set_ticklabels()]
+#        cbar.ax.label.set_color('white')
+#        [t.set_color('white') for t in cbar.ax.yaxis.set_tick_params()]
+#        cbar.ax.yaxis.set_tick_params(color='white')
+#        cbar.tick(color='white')
+#        cbar.outline.set_edgecolor('white')
+#        cbar.color = 'white'
+
+#        for tick in cbar.get_ticklabels():
+#            tick.set_color('red')
+
+# Uncomment the following text, as well as any future references to ax2, if a second plot is needed with the azimuthal averaged sigmad and four slices of the disc
+#        ax2 = fig.add_subplot(212)
+#        plt.figure()
+#        ax2 = plt.gca()
+#        ax2.set_xlabel('r')
+#        ax2.set_ylabel("dust surface density")
+#        sigmad_tot = sum(surfdens,axis=1)
+#        sigmad_azi = sigmad_tot/nphi
+
+
+        r_celledge = [None]*nrad
+        ncelledge = nrad+1
+#        print type(ncelledge), ncelledge
+
+#        filepath_r = 'link/used_rad.dat'
+#        f = open(filepath_r,'r')
+#        lines = f.readlines()
+#        print type(lines), lines
+
+#        for line in xrange(ncelledge-1):
+#            data = line.split()
+#            r_celledge.append(float(data[0]))
+#
+#            r_celledge.append(
+
+        rcell = [None] * (len(radii)-1)
+        for i in range(len(radii)-1):
+#            rcell[i] = (radii[i]*radii[i+1])**0.5
+            rcell[i] = (radii[i]+radii[i+1])/2.
+#        ax2.plot(rcell,sigmad_azi)
+
+        for i in range(4):
+            j = int(nphi/4.*i)
+#            print nx/4.*i, j
+            surfdensi = [None]*nrad
+            for k in range(nrad):
+#                print "inside k = ",k
+#                print surfdens
+#                surfdens = []
+#                print type(surfdens)                
+#                print surfdens[0,0], type(surfdens[0,0])
+#                sys.exit()
+#                surfdensi = float(surfdens[k,j])
+#                print surfdensi
+                surfdensi[k] = surfdens[k,j]
+#            print len(rcell), len(surfdensi)
+#            ax2.plot(rcell,surfdensi)
+
+        if (beam in ["yes", "y"]):
+            # Put the image in cartesian co-ordinates
+            interp = RectBivariateSpline(np.linspace(0,2*np.pi,nphi+1),rcell,dens_additional.T,kx=1,ky=1)
+            npix=2048
+            x_im=np.linspace(-3,3,npix)
+            y_im=np.linspace(-3,3,npix)
+            X_im,Y_im=np.meshgrid(x_im,y_im)
+            R_im=np.sqrt(X_im**2+Y_im**2)
+            Theta_im=np.arctan2(Y_im,X_im)+np.pi
+            image=interp(Theta_im,R_im,grid=False)
+
+            # Convolve with a gaussian and plot
+            sigma = 50.0
+            convolved=scipy.ndimage.filters.gaussian_filter(image,sigma)
+            fig3 = plt.figure()
+            ax3 = fig3.add_subplot(111) 
+#            plt.pcolormesh(x,y,log10(dens_additional.T),cmap=cm.Oranges_r,vmin=cbmin,vmax=cbmax)
+            plt.pcolormesh(-x_im,-y_im,np.log10(convolved),cmap=cm.Oranges_r,vmin=vmin,vmax=vmax)
+#            plt.contourf(image, levels=10)
+            ax3.set_xlim([-2.5,2.5])
+            ax3.set_ylim([-2.5,2.5])
+            cbar_label = 'log surface density [code units] (convolved)'
+            cbar = plt.colorbar()
+            cbar.set_label(cbar_label,color='black')
+
         if plot_gas in ["yes", "y"] and file_i == 0:
             plt.savefig(f'./images/gas_{simdir}_{outputnumber}.png', dpi=150)
         else:
@@ -265,7 +244,6 @@ if __name__ == "__main__":
     parser.add_argument('-d', metavar='dust_number',default=[], type=int, nargs="*" ,help="The dust type(s) \
 which is an integer")
     parser.add_argument('-cb', metavar='colourbar scale', type=float, nargs=2, default=[np.nan,np.nan], help="define the maximum and minimum colour bar scale")
-    parser.add_argument('-ptype', metavar='plot type', type=str, nargs=1, default="pcontour", help="type of plot")
     parser.add_argument('-lin', metavar='linear colour scale', type=str, nargs=1, default="no", help="linear plot")
     parser.add_argument('-gas', metavar='gas density', type=str, nargs=1, default="no", help="plot gas density")
     parser.add_argument('-con', metavar='convolved', type=str, nargs=1, default="no", help="convolved with a gaussian beam")
@@ -281,13 +259,6 @@ which is an integer")
 
     cbmin = args.cb[0]
     cbmax = args.cb[1]
-
-    if (args.ptype == 0.):
-        plot_type = args.ptype
-    else:
-        plot_type = args.ptype[0]
-
-    print("plot type = ",plot_type)
 
     if (args.lin == 0.):
         lin_scale = args.lin
@@ -318,4 +289,4 @@ which is an integer")
 #    rcParams["text.color"] = 'white'
 #    rcParams["text.fontsize"] = 14
 
-    plot_surf_dens(simdir,DUSTNUMS,Out,plot_type,lin_scale,cbmin,cbmax,gasdens,convolve,plot_planet)
+    plot_surf_dens(simdir,DUSTNUMS,Out,lin_scale,cbmin,cbmax,gasdens,convolve,plot_planet)
