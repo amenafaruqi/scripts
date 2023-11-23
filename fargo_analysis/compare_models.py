9d22e9e7d47774d578cf3a2202d1bc7aa2efad3f
Amena Faruqi <phrkvg@login01.avon.hpc>
2023-11-23 16:38:36 +0000

    Create empty script to populate with comparison plot code

---
+++ fargo_analysis/compare_models.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import argparse
+plt.style.use('default')
+
+# inputs: model names +dirs (2+), timesteps (same for both) (1+) 
+def overlay_gas_sigmas()
+    pass
+
+def sidebyside_contours()
+    pass
+
+def overlay_dustgasratio()
+    pass
+
