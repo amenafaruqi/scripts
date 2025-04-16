94dbca8c1fb60a9ed0e3c951f49dd72d53847ba8
Amena Faruqi <amena@cedar2.int.cedar.computecanada.ca>
2025-04-16 11:04:11 -0700

    Generate plots of pressure gradients vs planet mass

--- fargo_analysis/plot_ring_widths.py
+++ fargo_analysis/plot_ring_widths.py
@@ -10,9 +10,9 @@ from scipy.signal import find_peaks
 from numpy.polynomial import Polynomial
 import argparse
 
-# plt.rc('text', usetex=True)
-# plt.rc('font', family='serif')
-# mpl.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+mpl.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
 
 nstokes = 3
 cm = plt.get_cmap('viridis')
@@ -39,14 +39,12 @@ def find_ring_troughs(radii, sigma, i_peak):
     grad_li = 0
     while (grad_li < len(grad_left)-1) and (grad_left[grad_li]*grad_left[grad_li+1] >= 0):
         grad_li += 1
-        print(grad_li, len(grad_left))
     left_trough_i = i_peak - grad_li
 
     # check for change in gradient sign right of peak
     grad_ri = 0
     while (grad_ri < len(grad_right)-1) and (grad_right[grad_ri]*grad_right[grad_ri+1] >= 0):
         grad_ri += 1
-        print(grad_ri, len(grad_right))
     right_trough_i = i_peak + grad_ri
 
     return left_trough_i,  right_trough_i
@@ -112,8 +110,8 @@ def calculate_ring_widths():
 
 def calculate_pressure_gradients():
         # 1) Select data only within region close to planet
-        innerbound = rp + (3*r_hill)
-        outerbound = rp + (15*r_hill)  # search for peak from rp to outerbound
+        innerbound = rp + (5*r_hill)
+        outerbound = rp + (12*r_hill)  # search for peak from rp to outerbound
         innerbound_i = np.argmin(np.abs(radii-innerbound))      # index (radial cell number) of lower bound of peak search
         outerbound_i = np.argmin(np.abs(radii-outerbound))      # index (radial cell number) of upper bound of peak search
 
@@ -123,44 +121,44 @@ def calculate_pressure_gradients():
         # 2) Identify ring peaks and troughs in gas
         peak_i_bound = find_ring_peak(radii_bound,sigma_bound)
         peak_i = peak_i_bound + innerbound_i
-        peak_r = radii_bound[peak_i_bound]
 
-        left_trough_i, right_trough_i = find_ring_troughs(radii_bound, sigma_bound, peak_i_bound)
-        left_trough_i = left_trough_i + innerbound_i
-        right_trough_i = right_trough_i + innerbound_i
+        l_trough_bound_i, r_trough_bound_i = find_ring_troughs(radii_bound, sigma_bound, peak_i_bound)
+        l_trough_i = l_trough_bound_i + innerbound_i
+        r_trough_i = r_trough_bound_i + innerbound_i
 
+        # Plot gas rings to check pressure gradient calculations
         fig0, ax0 = plt.subplots(figsize=(7,5))
         ax0.cla()
         ax0.plot(radii,sigma_gas_1D/np.max(sigma_bound), c='k')
         ax0.scatter(radii,sigma_gas_1D/np.max(sigma_bound), c='k', marker='x')
-        ax0.scatter(radii[left_trough_i],(sigma_gas_1D/np.max(sigma_bound))[left_trough_i], c='r', marker='o')
-        ax0.scatter(radii[right_trough_i],(sigma_gas_1D/np.max(sigma_bound))[right_trough_i], c='r', marker='o')
+        ax0.scatter(radii[l_trough_i],(sigma_gas_1D/np.max(sigma_bound))[l_trough_i], c='r', marker='o')
+        ax0.scatter(radii[r_trough_i],(sigma_gas_1D/np.max(sigma_bound))[r_trough_i], c='r', marker='o')
+        ax0.scatter(radii[peak_i],(sigma_gas_1D/np.max(sigma_bound))[peak_i], c='y', marker='o')
         ax0.axvline(innerbound, c='k', linestyle='dashed')
         ax0.axvline(outerbound, c='k', linestyle='dashed')
         ax0.set_xlim(1,1.6)
-        # ax0.set_ylim(0,np.max(sigma_bound/np.max(sigma_bound))*1.05)
+        ax0.set_ylim(0,np.max(sigma_bound/np.max(sigma_bound))*1.2)
 
-        # 3) Calculate average dP/dr between peak and trough (outer part of ring)
-        r_outer_ring = radii[peak_i+1:right_trough_i-1]
-        sigma_outer_ring = sigma_gas_1D[peak_i+1:right_trough_i-1]
-        h_outer_ring = hr0*r_outer_ring**(f+1)
-        r_inner_ring = radii[left_trough_i+1:peak_i-1]
-        sigma_inner_ring = sigma_gas_1D[left_trough_i+1:peak_i-1]
-        h_inner_ring = hr0*r_inner_ring**(f+1)
-        pressure_ext = 4*(np.pi**2)*h_outer_ring*sigma_outer_ring*(r_outer_ring**(-3))
-        pressure_in = 4*(np.pi**2)*h_inner_ring*sigma_inner_ring*(r_inner_ring**(-3))
-        dPdr_ext = np.abs(np.gradient(pressure_ext, r_outer_ring))
-        dPdr_in = np.abs(np.gradient(pressure_in, r_inner_ring))
+        # 3) Calculate dP/dr
+        hr_bound = hr0*radii_bound**(f+1)
+        pressure = 4*(np.pi**2)*hr_bound*sigma_bound*(radii_bound**(-3))
+        dpdr = np.abs(np.gradient(pressure, radii_bound))
+        ax0.plot(radii_bound, dpdr/np.max(dpdr), c='r', label="$| \partial P/ \partial r |$")
+        # ax0.scatter(radii_bound, dpdr/np.max(dpdr), c='r')
+        ax0.legend()
 
-        ax0.plot(r_outer_ring,dPdr_ext, c='g')
-        ax0.plot(r_inner_ring,dPdr_in, c='b')
+        # Find steepest point interior/exterior to ring peak
+        dpdr_int = dpdr[l_trough_bound_i:peak_i_bound-1]   # don't look too close to peak
+        dpdr_ext = dpdr[peak_i_bound+1:r_trough_bound_i]
+
+        max_dpdr_int = np.max(dpdr_int)
+        max_dpdr_ext = np.max(dpdr_ext)
+        ax0.plot(radii_bound[l_trough_bound_i:peak_i_bound-1], dpdr_int/np.max(dpdr), c='b')
+        ax0.plot(radii_bound[peak_i_bound+1:r_trough_bound_i], dpdr_ext/np.max(dpdr), c='g')
         fig0.savefig(f"{plots_savedir}/gasrings_{mp}Me_{hr0}_.png")
 
-        avg_dpdr_ext = np.max(dPdr_ext)  # mean or sum ???
-        avg_dpdr_in = np.max(dPdr_in)  # mean or sum ???
-        dpdrs_ext[s] = avg_dpdr_ext
-        dpdrs_in[s] = avg_dpdr_in
-
+        dpdrs_ext[s] = max_dpdr_ext
+        dpdrs_in[s] = max_dpdr_int
 
 
 # ============== Read in data from models ==============
@@ -168,9 +166,9 @@ def calculate_pressure_gradients():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Fit dust rings', prefix_chars='-')
 
-    parser.add_argument('-wd', metavar='wd', type=str, nargs=1, default=["/home/astro/phrkvg/simulations/dusty_fargo_models"],help="working directory containing simulations")
+    parser.add_argument('-wd', metavar='wd', type=str, nargs=1, default=["/home/amena/scratch/simulations/dusty_fargo/h0.05"],help="working directory containing simulations")
     parser.add_argument('-sims', metavar='sim', type=str, nargs="*", default=["10Me"] ,help="simulation directory containing output files")
-    parser.add_argument('-savedir', metavar='savedir', type=str, nargs=1, default="./images/ringfitting/" ,help="directory to save plots to")
+    parser.add_argument('-savedir', metavar='savedir', type=str, nargs=1, default="/home/amena/scratch/images/" ,help="directory to save plots to")
     parser.add_argument('-o', metavar='outputs',default=[600], type=int, nargs="*" ,help="outputs to plot")
     parser.add_argument('-plot_window', action="store_true")
     parser.add_argument('-style', metavar='style', type=str, nargs="*", default=["publication"] ,help="style sheet to apply to plots")
@@ -282,7 +280,7 @@ if __name__ == "__main__":
     
 
     # ================ Generate chosen plots ================
-    print(f"-------------------\nPlotting output {output} for {sim}\n=============")
+    print(f"-------------------\nPlotting output {output} for {sims}\n=============")
 
     if "rwidth" in plots:
         # Plot ring width vs planet mass
@@ -305,15 +303,15 @@ if __name__ == "__main__":
     if "dpdr" in plots:
         # Plot avg pressure gradient vs planet mass
         print("Plotting dP/dr against planet mass....")
-        ax_p.scatter(planet_masses, dpdrs_ext, c='b')
-        ax_p.plot(planet_masses, dpdrs_ext, c='b')
-        ax_p.scatter(planet_masses, dpdrs_in, c='r')
-        ax_p.plot(planet_masses, dpdrs_in, c='r')
+        ax_p.scatter(planet_masses, dpdrs_ext, c='g')
+        ax_p.plot(planet_masses, dpdrs_ext, c='g', label = "exterior to ring peak")
+        ax_p.scatter(planet_masses, dpdrs_in, c='b')
+        ax_p.plot(planet_masses, dpdrs_in, c='b', label="interior to ring peak")
         # ax_p.set_ylim(-0.05,0.7)
         ax_p.set_xlabel("Planet mass (M$_\oplus$)")
         ax_p.set_ylabel("$ |\partial P/\partial r |$")
         ax_p.set_title(f"H/R = {hr0}")
-        # ax_p.legend()
+        ax_p.legend()
         fig_p.savefig(f"{plots_savedir}/pressure_grad_{hr0}.png", dpi=200)
 
     if plot_window:
